c0de6a01195693b0977a402c33b0675440586373
Ayush Kumar <ayush18111998@gmail.com>
2025-12-18 14:59:17 +0530

    string and array manipulation techniques

--- main.js
+++ main.js
@@ -5,15 +5,47 @@
 
 //strings , numbers , boolean , null , undefined
 
-const name = "ayush";
-const age = 21;
-const isgood = false;
-const a = null;
-const b = undefined;
-let c;
+// const name = "ayush";
+// const age = 21;
+// const isgood = false;
+// const a = null;
+// const b = undefined;
+// let c;
 
-console.log(typeof (age));
-console.log(typeof (name));
-console.log(typeof (isgood));
-console.log(typeof (a));
-console.log(typeof (b));
+// console.log(typeof (age));
+// console.log(typeof (name));
+// console.log(typeof (isgood));
+// console.log(typeof (a));
+// console.log(typeof (b));
+
+
+//CONCATENATION
+
+// console.log('My name is ' + name + ' and I am ' + age + ' years old.')
+// console.log(`My name is ${name} and I am ${age} years old.`)
+
+
+//string properties and methods
+
+// const s = "hello world hehehahaokok";
+// console.log(s.length);
+// console.log(s.toUpperCase());
+// console.log(s.toLocaleLowerCase());
+// console.log(s.split(' '))
+
+
+//// array manipulation
+
+// const fruit = ['apple', 'mango', 'guava'];
+// console.log(fruit);
+// console.log(fruit[1]);
+
+// fruit[3] = 'banana'
+
+// console.log(fruit[3]);
+
+// fruit.push('pineapple'); // push => make item added to array but at the END
+// console.log(fruit);
+
+// fruit.unshift('watermelon'); // unshit => push => make item added to array but at the START
+// console.log(fruit);
