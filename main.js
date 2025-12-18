9bdf6f6620a20b236defc7d1d93a5cb42de9b01d
Ayush Kumar <ayush18111998@gmail.com>
2025-12-18 12:17:59 +0530

    primitive data types

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-console.log("hi");
-console.error("this is error");
-console.warn("warning");
+// console.log("hi");
+// console.error("this is error");
+// console.warn("warning");
+
+
+//strings , numbers , boolean , null , undefined
+
+const name = "ayush";
+const age = 21;
+const isgood = false;
+const a = null;
+const b = undefined;
+let c;
+
+console.log(typeof (age));
+console.log(typeof (name));
+console.log(typeof (isgood));
+console.log(typeof (a));
+console.log(typeof (b));
