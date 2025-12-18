a1157b2ad60aa806f9d16f034c4d637559f690b2
Ayush Kumar <ayush18111998@gmail.com>
2025-12-18 15:57:32 +0530

    object literals

--- main.js
+++ main.js
@@ -49,3 +49,41 @@
 
 // fruit.unshift('watermelon'); // unshit => push => make item added to array but at the START
 // console.log(fruit);
+
+
+// object literals
+
+
+// const person = {
+//     firstname: 'Ayush', lastname: 'Kumar', age: 21, hobbies: ['sports', 'gaming', 'eating'], address: {
+//         street: '50 main st', city: 'delhi', country: 'india', pincode : 101010  }
+  
+// }
+
+// console.log(person.firstname);
+// console.log(person.address);
+// console.log(person.address.pincode);
+// console.log(person.hobbies[2]);
+
+// console.log(`my name is ${person.firstname} and i am ${person.age} , my hobbies are ${person.hobbies}`)
+
+// person.gmail = 'person@gmail.com';
+// console.log(person.gmail);
+
+
+
+// array object literals
+
+const todos = [
+    { id: 1, agenda: 'meeting with boss', isCompleted: true } ,
+    { id: 2, agenda: 'coffee', isCompleted: true } ,
+    { id: 3, agenda: 'book reading', isCompleted: false }
+]
+
+// console.log(todos[0].agenda, todos[0].isCompleted , todos[2].agenda , todos[2].isCompleted);
+
+
+//to convert data into json 
+
+// const todoJSON = JSON.stringify(todos);
+// console.log(todoJSON);
